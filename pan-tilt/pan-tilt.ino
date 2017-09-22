81eb35b5fa1fe65544af1876912ad077c9c40b4a
Benjamin Lilly <blilly@olin.edu>
2017-09-22 11:47:11 -0400

    Added IR library and wait for serial character 1

--- pan-tilt/pan-tilt.ino
+++ pan-tilt/pan-tilt.ino
@@ -1,40 +1,57 @@
 //By setting azimuthLower and azimuthUpper, you choose how far left and right the sensor pans while scannning.
 //By setting elevationLower and elevationUpper, you choose how high and low the sensor tilts while scanning. elevationLower and elevationUpper both have to be even or zero.
+//azimuthLower and azimuthUpper must have a difference that's a multiple of azimuthStep
+//elevationLower and elevationUpper must have a difference that's a multiple of elevationStep
+
 
  #include "Servo.h"
+
+ #include <SharpIR.h>
 
 Servo pan;
 Servo tilt;
 
 int azimuthLower = 0;
 int azimuthUpper = 180;
+int azimuthStep = 5;
 
-int elevationLower = 0;
-int elevationUpper = 180;
+int elevationLower = 45;
+int elevationUpper = 180-45;
+int elevationStep=5;
 
-int voltage
+int distance;
+
+SharpIR sharp(A0, 20150);
 
 void setup() {
 
+  Serial.begin(9600);
   pan.attach(10);
   tilt.attach(9);
+
+  while(Serial.read()!='1'){
+  }
+  
 }
 
+
 void loop() {
-  for (int elevation=elevationLower; elevation=elevationUpper; elevation=elevation+2) {
-    tilt.write(elevation);
-    delay(53);
-    for (int azimuth=azimuthLower; azimuth=azimuthUpper; azimuth++) {
-      pan.write(azimuth);
-      delay(53);
-      println(String(azimuth) "," String(elevation) "," String(analogRead(A0))) 
+  for (int azimuth=azimuthLower; azimuth<=azimuthUpper; azimuth=azimuth+2*azimuthStep) {
+    tilt.write(azimuth);
+    delay(200);
+    for (int elevation=elevationLower; elevation<=elevationUpper; elevation=elevation+elevationStep) {
+      pan.write(elevation);
+      delay(50);
+      Serial.println(String(azimuth) + "," + String(elevation) + "," + String(sharp.distance())); 
     }
-    tilt.write(elevation+1);
-    delay(53);
-    for (int (azimuth=azimuthUpper; azimuth=azimuthLower; azimuth--) {
-      pan.write(azimuth);
-      delay(53);
-      println(String(azimuth) "," String(elevation) "," String(analogRead(A0))) 
+    tilt.write(azimuth+azimuthStep);
+    delay(200);
+    for (int elevation=elevationUpper; elevation>=elevationLower; elevation=elevation-elevationStep) {
+      pan.write(elevation);
+      delay(50);
+      distance=sharp.distance();
+      Serial.println(String(azimuth+azimuthStep) + "," + String(elevation) + "," + String(sharp.distance()));
     }
   }
-
+delay(750); 
+}  
