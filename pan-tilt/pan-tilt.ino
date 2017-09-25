75f4abb2762d9b0970e4c988c122edbaa1993c5f
Benjamin Lilly <blilly@olin.edu>
2017-09-24 23:16:36 -0400

    Converted Arduino sketch to give degrees off center rather than pixels

--- pan-tilt/pan-tilt.ino
+++ pan-tilt/pan-tilt.ino
@@ -11,15 +11,29 @@
 Servo pan;
 Servo tilt;
 
-int azimuthLower = 0;
-int azimuthUpper = 180;
-int azimuthStep = 20;
+//int azimuthLower = 0;
+//int azimuthUpper = 180;
+//int azimuthStep = 20;
 
-int elevationLower = 45;
-int elevationUpper = 180-45;
-int elevationStep = 20;
+int azimuthCenter=90;
+int azimuthSweep=45;
+int azimuthStep=5;
+int azimuthLower=azimuthCenter-azimuthSweep;
+int azimuthUpper=azimuthCenter+azimuthSweep;
+
+//int elevationLower = 45;
+//int elevationUpper = 180-45;
+//int elevationStep = 20;
+
+int elevationCenter=90;
+int elevationSweep=45;
+int elevationStep=5;
+int elevationLower=elevationCenter-elevationSweep;
+int elevationUpper=elevationCenter+elevationSweep;
 
 int distance;
+int azimuthDegreesOffCenter;
+int elevationDegreesOffCenter;
 
 SharpIR sharp(A0, 20150);
 
@@ -28,31 +42,44 @@ void setup() {
   Serial.begin(9600);
   pan.attach(10);
   tilt.attach(9);
-
-
-  
 }
-
 
 void loop() {
 
-  while(Serial.read()!='1'){} //Wait for a prompt from Python
+  //while(Serial.read()!='1'){} //Wait for a prompt from Python
   
-  for (int azimuth=azimuthLower; azimuth<=azimuthUpper; azimuth=azimuth+2*azimuthStep) {
+  for (int azimuth=azimuthLower; azimuth<=azimuthUpper; azimuth=azimuth+azimuthStep) {
     tilt.write(azimuth);
     delay(200);
     for (int elevation=elevationLower; elevation<=elevationUpper; elevation=elevation+elevationStep) {
       pan.write(elevation);
       delay(50);
-      Serial.println(String(azimuth) + "," + String(elevation) + "," + String(sharp.distance())); 
-    }
-    tilt.write(azimuth+azimuthStep);
-    delay(200);
-    for (int elevation=elevationUpper; elevation>=elevationLower; elevation=elevation-elevationStep) {
-      pan.write(elevation);
-      delay(50);
       distance=sharp.distance();
-      Serial.println(String(azimuth+azimuthStep) + "," + String(elevation) + "," + String(sharp.distance()));
+      if(distance>150){
+        distance=150;
+      }
+      azimuthDegreesOffCenter=azimuth-azimuthCenter;
+      elevationDegreesOffCenter=elevation-elevationCenter;
+      //Serial.println(String(azimuth) + "," + String(elevation) + "," + String(sharp.distance())); 
+      Serial.println(String(azimuthDegreesOffCenter) + "," + String(elevationDegreesOffCenter) + "," + String(distance));
+    }
+    azimuth=azimuth+azimuthStep;
+    //tilt.write(azimuth+azimuthStep);
+    tilt.write(azimuth);
+    delay(200);
+    if(azimuth<azimuthUpper){ //Skips last downward tilt if azimuthUpper has already been reached
+      for (int elevation=elevationUpper; elevation>=elevationLower; elevation=elevation-elevationStep) {
+        pan.write(elevation);
+        delay(50);
+        distance=sharp.distance();
+        if(distance>150){
+          distance=150;
+        }
+        azimuthDegreesOffCenter=azimuth-azimuthCenter;
+        elevationDegreesOffCenter=elevation-elevationCenter;
+        //Serial.println(String(azimuth+azimuthStep) + "," + String(elevation) + "," + String(sharp.distance()));
+        Serial.println(String(azimuthDegreesOffCenter) + "," + String(elevationDegreesOffCenter) + "," + String(distance));
+      }  
     }
   }
   Serial.println("-1,-1,-1"); //Done!
